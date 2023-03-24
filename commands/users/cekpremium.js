2b607128598d9f57454ae288bd8a9053561c1ab0
Hikari Kawai <121547505+hikarikawai@users.noreply.github.com>
2023-03-25 06:33:26 +0700

    Update cekpremium.js

--- commands/users/cekpremium.js
+++ commands/users/cekpremium.js
@@ -5,7 +5,7 @@ module.exports = {
     type: "users",
     example: "%prefix%command",
     isPremium: true,
-    noLimit: true,
+    noLimit: false,
     start: async(killua, m) => {
         let cekprem = require("parse-ms")((await user.getPremiumExpired(m.sender, _user)) - Date.now())
         let caption = `*Expired :* ${cekprem.days} day ${cekprem.hours} hour ${cekprem.minutes} minute ${cekprem.seconds} Second`
